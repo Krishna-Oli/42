258eec497e5b4e07cefdbc2b5b4f6d39b2f45c50
Krishna Oli <koli@e1z2r5p30.42.us.org>
2018-11-18 04:57:52 -0800

    hopefully works now

---
+++ Get_Next_Line/libft/ft_memchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: koli <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/09 20:07:39 by koli              #+#    #+#             */
+/*   Updated: 2018/11/09 20:10:18 by koli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *ptr;
+
+	ptr = (unsigned char*)s;
+	while (n--)
+	{
+		if (*ptr != (unsigned char)c)
+			ptr++;
+		else
+			return (ptr);
+	}
+	return (0);
+}
---
+++ Get_Next_Line/libft/ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: koli <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/09 21:21:06 by koli              #+#    #+#             */
+/*   Updated: 2018/11/09 21:21:58 by koli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*new_str;
+	unsigned int	i;
+
+	if (!s)
+		return (NULL);
+	new_str = ft_strnew(ft_strlen(s));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new_str[i] = f(i, s[i]);
+		i++;
+	}
+	return (new_str);
+}
